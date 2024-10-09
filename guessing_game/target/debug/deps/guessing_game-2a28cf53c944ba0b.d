c803eca7b9ab6a5d1c135445c4fbb2053ed2c3e5
TuinderJ <joshua.tuinder@gmail.com>
2024-10-09 07:05:01 -0600

    learning about importing libraries, using functions from types, variable creation and referencing, and basic error handling

---
+++ guessing_game/target/debug/deps/guessing_game-2a28cf53c944ba0b.d
@@ -0,0 +1,5 @@
+/home/josh/rust/rust_book/guessing_game/target/debug/deps/libguessing_game-2a28cf53c944ba0b.rmeta: src/main.rs
+
+/home/josh/rust/rust_book/guessing_game/target/debug/deps/guessing_game-2a28cf53c944ba0b.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/josh/rust/rust_book/guessing_game/target/debug/guessing_game: /home/josh/rust/rust_book/guessing_game/src/main.rs
